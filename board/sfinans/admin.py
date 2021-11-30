21be093d4529b56d1ba6ebc3cd9997661e180298
maxim <anapa.igor@yandex.ru>
2021-11-30 17:01:08 +0300

    -add new models;

--- board/sfinans/admin.py
+++ board/sfinans/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
 
-# Register your models here.
+from sfinans import models
+
+
+@admin.register(models.Group)
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+
+@admin.register(models.Chapter)
+class ChapterAdmin(admin.ModelAdmin):
+    list_display = ['title', 'description']
+
+
+@admin.register(models.Subchapter)
+class SubchapterAdmin(admin.ModelAdmin):
+    list_display = ['title', 'description']
+
+
+@admin.register(models.DocumentStatus)
+class DocumentStatusAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+
+@admin.register(models.Document)
+class DocumentAdmin(admin.ModelAdmin):
+    list_display = ['name', 'status']
+
+
+@admin.register(models.GroupChapter)
+class GroupChapterAdmin(admin.ModelAdmin):
+    list_display = ['group', 'chapter']
+
+
+@admin.register(models.ChapterSubchapter)
+class ChapterSubchapterAdmin(admin.ModelAdmin):
+    list_display = ['chapter', 'subchapter']
+
+
+@admin.register(models.ChapterSubchapterDocument)
+class ChapterSubchapterDocumentAdmin(admin.ModelAdmin):
+    list_display = ['chapter', 'subchapter', 'document']
--- board/sfinans/models.py
+++ board/sfinans/models.py
@@ -1,3 +1,125 @@
 from django.db import models
 
-# Create your models here.
+
+class Group(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Группы'
+        verbose_name = 'Группа'
+
+
+class Chapter(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(max_length=1000)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = 'Разделы'
+        verbose_name = 'Подразделы'
+
+
+class Subchapter(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(max_length=1000)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = 'Подразделы'
+        verbose_name = 'Подраздел'
+
+
+class DocumentStatus(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Статусы документа'
+        verbose_name = 'Статус документа'
+
+
+class Document(models.Model):
+    name = models.CharField(max_length=100)
+    status = models.ForeignKey(
+        DocumentStatus,
+        on_delete=models.PROTECT,
+        default='',
+        related_name='document',
+        verbose_name='Статус',
+    )
+
+
+class GroupChapter(models.Model):
+    group = models.ForeignKey(
+        Group,
+        on_delete=models.CASCADE,
+        default='',
+        related_name='group_chapter',
+        verbose_name='Группа',
+    )
+    chapter = models.ForeignKey(
+        Chapter,
+        on_delete=models.CASCADE,
+        default='',
+        related_name='group_chapter',
+        verbose_name='Раздел',
+    )
+
+    def __str__(self):
+        return f'{self.group} - {self.chapter}'
+
+
+class ChapterSubchapter(models.Model):
+    chapter = models.ForeignKey(
+        Chapter,
+        on_delete=models.CASCADE,
+        default='',
+        related_name='chapter_subchapter',
+        verbose_name='Раздел',
+    )
+    subchapter = models.ForeignKey(
+        Subchapter,
+        on_delete=models.CASCADE,
+        default='',
+        related_name='chapter_subchapter',
+        verbose_name='Подгруппа',
+    )
+
+    def __str__(self):
+        return f'{self.chapter} - {self.subchapter}'
+
+
+class ChapterSubchapterDocument(models.Model):
+    chapter = models.ForeignKey(
+        Chapter,
+        on_delete=models.CASCADE,
+        default='',
+        related_name='chapter_subchapter_document',
+        verbose_name='Раздел',
+    )
+    subchapter = models.ForeignKey(
+        Subchapter,
+        on_delete=models.CASCADE,
+        default='',
+        related_name='chapter_subchapter_document',
+        verbose_name='Подгруппа',
+    )
+    document = models.ForeignKey(
+        Document,
+        on_delete=models.CASCADE,
+        default='',
+        related_name='chapter_subchapter_document',
+        verbose_name='Документ',
+    )
+
+    def __str__(self):
+        return f'{self.chapter} - {self.subchapter} - {self.document}'
